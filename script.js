2215cd5a2d227b294a7b27195ae98c5b33a16708
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-03-28 15:00:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+    const circles = document.querySelectorAll('.circle');
+    const nextBtn = document.getElementById('next');
+    const prevBtn = document.getElementById('prev');
+    let currentActive = 0;
+
+    updateButtons();
+
+    nextBtn.addEventListener('click', () => {
+        currentActive++;
+
+        if (currentActive > circles.length - 1) {
+            currentActive = circles.length - 1;
+        }
+
+        updateProgress();
+        updateButtons();
+    });
+
+    prevBtn.addEventListener('click', () => {
+        currentActive--;
+
+        if (currentActive < 0) {
+            currentActive = 0;
+        }
+
+        updateProgress();
+        updateButtons();
+    });
+
+    function updateProgress() {
+        circles.forEach((circle, idx) => {
+            if (idx <= currentActive) {
+                circle.classList.add('active');
+            } else {
+                circle.classList.remove('active');
+            }
+        });
+    }
+
+    function updateButtons() {
+        if (currentActive === 0) {
+            prevBtn.disabled = true;
+        } else {
+            prevBtn.disabled = false;
+        }
+
+        if (currentActive === circles.length - 1) {
+            nextBtn.disabled = true;
+        } else {
+            nextBtn.disabled = false;
+        }
+    }
+});
